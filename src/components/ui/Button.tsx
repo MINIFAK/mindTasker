381a274ebd31a9543993c40d1b042c9c1fb111f4
Lucas Kauã <minifakdev15@gmail.com>
2024-12-10 16:33:45 -0300

    ✨ feat: Nova variant Text e melhorias no css do Button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,23 +4,27 @@ import Link from "next/link";
 import { tv, VariantProps } from "tailwind-variants";
 
 const buttonVariant = tv({
-  base: "flex items-center rounded-2xl gap-2 p-3 justify-center transition-colors duration-300 ease-in-out",
+  base: "flex items-center rounded-3xl gap-2 p-3 justify-center transition-colors duration-300 ease-in-out",
   variants: {
     variant: {
-      default: "bg-primary-500 text-neutral-900 font-inter font-medium  hover:bg-primary-400 active:bg-primary-600 disabled:bg-primary-300 disabled:text-neutral-400 disabled:cursor-not-allowed", 
-      outline: "outline outline-2 outline-primary-500 text-primary-500 font-inter font-medium hover:bg-primary-400 hover:text-white hover:outline-none active:bg-primary-600 active:outline-none disabled:outline-nonoe disabled:outline-primary-300 disabled:text-primary-300 disabled:cursor-not-allowed", 
-      floating: "bg-primary-500 hover:bg-primary-400 active:bg-primary-600 disabled:bg-primary-300 disabled:cursor-not-allowed"
+      default:
+        "bg-primary-500 text-neutral-900 font-inter font-medium  hover:bg-primary-400 active:bg-primary-600 disabled:bg-primary-300 disabled:text-neutral-400 disabled:cursor-not-allowed",
+      outline:
+        "outline outline-2 outline-primary-500 text-primary-500 font-inter font-medium hover:bg-primary-400 hover:text-white hover:outline-none active:bg-primary-600 active:outline-none disabled:outline-nonoe disabled:outline-primary-300 disabled:text-primary-300 disabled:cursor-not-allowed",
+      floating:
+        "bg-primary-500 hover:bg-primary-400 active:bg-primary-600 disabled:bg-primary-300 disabled:cursor-not-allowed",
+      text: "text-neutral-900 font-inter font-medium  hover:bg-primary-400 active:bg-primary-600 disabled:bg-primary-300 disabled:text-neutral-400 disabled:cursor-not-allowed",
     },
     size: {
       none: "",
       normal: "min-h-8 min-w-28",
       full: "w-full",
-      floating: "min-w-10 min-h-10 rounded-full p-3"
+      floating: "min-w-10 min-h-10 rounded-full",
     },
   },
   defaultVariants: {
     variant: "default",
-    size: "normal"
+    size: "normal",
   },
 });
 type ButtonProps = VariantProps<typeof buttonVariant> & {
@@ -42,7 +46,7 @@ export function Button({
   size,
   ...rest
 }: ButtonProps) {
-  const classname = buttonVariant({ variant, size, className})
+  const classname = buttonVariant({ variant, size, className });
   if (type === "a") {
     return (
       <a
@@ -83,7 +87,7 @@ export function Button({
   if (type === "buttonSubmit") {
     return (
       <button
-       type="submit"
+        type="submit"
         className={classname}
         {...(rest as React.ButtonHTMLAttributes<HTMLButtonElement>)}
       >
@@ -91,6 +95,5 @@ export function Button({
       </button>
     );
   }
-
   return null;
 }
