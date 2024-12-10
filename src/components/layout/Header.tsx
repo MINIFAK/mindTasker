5966cad0f04a138014f1091a02aa272e44eadb8d
Lucas Kauã <minifakdev15@gmail.com>
2024-12-10 16:37:19 -0300

    ✨ feat: Melhoria na responsividade do Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,20 +1,21 @@
-import Image from "next/image"
-import Clock from "../ui/Clock"
+import Image from "next/image";
+import Clock from "../ui/Clock";
 
-export const Header = () => { 
-  return(
-    <header className="p-7 flex justify-between">
-      <Image 
-       src="/MindTaskerLight.svg" 
-       alt="Logo do MindTasker" 
-       width={230}              
-       height={44}             
-       priority    
-      /> 
-      <div className="flex gap-12 items-center justify-center">
-        <Clock/>
+export const Header = () => {
+  return (
+    <header className="p-2 sm:p-6 flex justify-between">
+      <div className="relative w-48 sm:w-60 h-11">
+        <Image
+          src="/MindTaskerLight.svg"
+          alt="Logo do MindTasker"
+          priority
+          fill
+        />
+      </div>
+      <div className="flex gap-4 sm:gap-12 items-center justify-center">
+        <Clock />
         <div className="w-12 h-12 bg-primary-950 rounded-full"></div>
       </div>
     </header>
-  )
-}
+  );
+};
