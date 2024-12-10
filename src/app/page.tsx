70962d9fd61bbf844380b848469b97afa23625d5
Lucas Kauã <minifakdev15@gmail.com>
2024-12-10 16:38:15 -0300

    ✨ feat: Finalização do UI da Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import { Header } from "@/components/layout/Header";
 import { Button } from "@/components/ui/Button";
 import CardTask from "@/components/ui/CardTask";
+import HorizontalScroller from "@/components/ui/HorizontalScroller";
+import Timer from "@/components/ui/Time";
 import { PlusIcon } from "lucide-react";
-
 
 const Projects = [
   "Aprender Inglês",
@@ -14,8 +15,8 @@ const Projects = [
   "Explorar Cloud Computing",
   "Entender Banco de Dados",
   "Desenvolver Portfólio",
-  "Aprimorar Soft Skills"
-]
+  "Aprimorar Soft Skills",
+];
 const Tasks = [
   "Revisar conteúdo aprendido",
   "Praticar exercícios diários",
@@ -26,32 +27,113 @@ const Tasks = [
   "Participar de comunidades",
   "Resolver desafios práticos",
   "Escrever resumos teóricos",
-  "Testar conhecimentos com quizzes"
-]
-
+  "Testar conhecimentos com quizzes",
+];
 
 export default function Home() {
   return (
     <>
-      <Header/>
-      <main className="p-2 sm:px-28 mt-10">
-
-        <h1 className="font-poppins text-5xl font-medium text-neutral-800 ">Seja Bem Vindo <span className="font-bold text-primary-600">MINIFAK</span></h1>
-
-        <section className="mt-8 p-1">
-          <h2 className="font-poppins text-neutral-800 font-semibold text-4xl mb-2">Meus Projetos</h2>
-          <div className="flex gap-4">
-            <CardTask data={Projects}/>
+      <Header />
+      <main className="p-2 xl:px-28 mt-10">
+        <h1 className="font-poppins text-4xl sm:text-5xl font-medium text-neutral-800 ">
+          Seja Bem Vindo
+          <span className="font-bold text-primary-600 ml-3">MINIFAK !</span>
+        </h1>
+        <section className="mt-10 sm:mt-8 ml-1">
+          <h2 className="font-poppins text-neutral-800 font-semibold text-3xl sm:text-4xl mb-2">
+            Meus Projetos
+          </h2>
+          <div className="flex gap-4 px-1">
+            <Button
+              type="button"
+              className="min-w-24 min-h-24"
+              variant="floating"
+              size="floating"
+            >
+              <PlusIcon className="w-12 h-12 text-white" />
+            </Button>
+            <HorizontalScroller>
+              {Projects.map((name, index) => (
+                <CardTask name={name} key={index} />
+              ))}
+            </HorizontalScroller>
           </div>
         </section>
-
-        <section className="mt-8 px-5">
-          <h2 className="font-poppins text-neutral-800 font-semibold text-4xl mb-2">Minhas Tarefas</h2>
-          <div className="flex gap-4">
-            <CardTask data={Tasks}/>
+        <section className="mt-8 ml-5">
+          <h2 className="font-poppins text-neutral-800 font-semibold text-3xl sm:text-4xld mb-2">
+            Minhas Tarefas
+          </h2>
+          <div className="flex gap-4 px-1">
+            <Button
+              type="button"
+              className="min-w-24 min-h-24"
+              variant="floating"
+              size="floating"
+            >
+              <PlusIcon className="w-12 h-12 text-white" />
+            </Button>
+            <HorizontalScroller>
+              {Tasks.map((name, index) => (
+                <CardTask name={name} key={index} />
+              ))}
+            </HorizontalScroller>
           </div>
         </section>
+        <section className="mt-8 ml-10 mr-5 sm:flex sm:justify-between">
+          <div>
+            <h2 className="font-poppins text-neutral-800 font-semibold text-4xl mb-2">
+              Tarefa em Foco
+            </h2>
+            <div className="px-1">
+              <h3 className="font-inter text-primary-500 font-medium text-2xl mb-2">
+                Estudar Back End
+              </h3>
 
+              <div className="flex gap-3 my-5">
+                <Button type="button"> Iniciar Cronometro</Button>
+                <Button type="button" variant="text">
+                  Ver gráfico
+                </Button>
+              </div>
+
+              <div className="px-1 font inter text-lg">
+                <p>
+                  Hoje, o total de foco na tarefa foi de{" "}
+                  <strong className="text-primary-500 font-bold">
+                    2 horas 16 minutos e 30 segundos
+                  </strong>
+                </p>
+                <p>
+                  Nesta semana, o total de foco na tarefa foi de{" "}
+                  <strong className="text-primary-500 font-bold">
+                    15 horas 5 minutos e 30 segundos
+                  </strong>
+                </p>
+                <p>
+                  Neste mês, o total de foco na tarefa foi de{" "}
+                  <strong className="text-primary-500 font-bold">
+                    30 horas 25 minutos
+                  </strong>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="text-center my-8 sm:my-0">
+            <h2 className="font-poppins text-neutral-800 font-semibold text-4xl mb-2">
+              Conquiste o Foco
+            </h2>
+            <p className="mb-4">
+              Desmotre e conquiste o foco nessa tarefa com um total de
+              <strong className="text-primary-500"> 100 </strong>
+              horas
+            </p>
+            <div className="flex items-center justify-center">
+              <Timer current={50} max={100}>
+                <p className="font-inter text-5xl font-semibold">50</p>
+              </Timer>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
---
+++ src/components/ui/Time.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import React, { Children } from "react";
+
+interface TimerProps {
+  max: number;
+  current: number;
+  size?: number;
+  children?: React.ReactNode;
+}
+
+const Timer: React.FC<TimerProps> = ({
+  max,
+  current,
+  size = 192,
+  children,
+}) => {
+  const progress = (current / max) * 100;
+  const radius = size / 2 - 10;
+  const circumference = 2 * Math.PI * radius;
+
+  return (
+    <div
+      className="relative flex flex-col items-center justify-center"
+      style={{ width: size, height: size }}
+    >
+      <svg
+        className="transform -rotate-90"
+        width={size}
+        height={size}
+        viewBox={`0 0 ${size} ${size}`}
+      >
+        <circle
+          cx={size / 2}
+          cy={size / 2}
+          r={radius}
+          strokeWidth={size * 0.05}
+          className="text-neutral-400"
+          fill="transparent"
+          stroke="currentColor"
+        />
+        {/* Progresso */}
+        <circle
+          cx={size / 2}
+          cy={size / 2}
+          r={radius}
+          strokeWidth={size * 0.05}
+          fill="transparent"
+          className="text-primary-500"
+          stroke="currentColor"
+          strokeDasharray={circumference}
+          strokeDashoffset={circumference - (circumference * progress) / 100}
+          style={{
+            transition: "stroke-dashoffset 0.5s ease-in-out",
+          }}
+        />
+      </svg>
+      <div className="absolute text-black font-bold">{children}</div>
+    </div>
+  );
+};
+
+export default Timer;
