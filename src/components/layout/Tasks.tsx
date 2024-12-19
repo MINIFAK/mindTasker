3b3611708c10890601d84da1f63c9c085a70e6e3
Lucas Kauã <minifakdev15@gmail.com>
2024-12-19 11:36:20 -0300

    ✨ feat: Finalização do Tasks da HOME ( Dashboard)

--- src/components/layout/Tasks.tsx
+++ src/components/layout/Tasks.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { PlusIcon } from "lucide-react";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 
 import { Button } from "../ui/Button";
@@ -10,6 +10,12 @@ import CardTask from "../ui/CardTask";
 
 import { useRouter, useSearchParams } from "next/navigation";
 import { TaskDetail } from "./TaskDetail";
+import { CreateTask } from "../ui/CreateTask";
+import useContextMenu from "@/hook/useContextMenu";
+import { toast } from "sonner";
+import { ContextMenu, ContextMenuButton } from "../ui/contextMenu";
+import { EditTask } from "../ui/EditTask";
+import { DeleteTask } from "../ui/DeleteTask";
 
 export interface TasksDataProps{
   projectId: string
@@ -20,19 +26,38 @@ export interface TasksDataProps{
   month: number
 }
 
-export interface TasksProps{
-  data: TasksDataProps[]
-}
-export function Tasks({
-  data: Data
-}: TasksProps){
+export function Tasks(){
+  const [data, setData] = useState<TasksDataProps[]>([])
+  const [openEdit, setOpenEdit] = useState(false)
+  const [openDelete, setOpenDelete] = useState(false)
 
-  const [data, setData] = useState<TasksDataProps[]>(Data)
+
+  const { menuRef,handleContextMenu, visible,position } = useContextMenu()
   const router = useRouter()
   const searchParams = useSearchParams()
 
+  const currentProject  = searchParams.get('project')
   const currentTask  = searchParams.get('task')
 
+   useEffect(()=> {
+    const getData = async() => {
+        setData(await fetch(`${process.env.NEXT_PUBLIC_BASE_URL_API}/api/tasks/${currentProject}`, {
+          method: "GET",
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include', 
+          next: { revalidate: 60 }
+        }).then((res) => res.json()).then((data)=> {
+          return data.message ? [] : data
+        }).catch(error => {
+          console.error('[Erro ao buscar dados das tarefa:', error);
+          return []
+        }))
+      }
+      getData()
+    }, [currentProject])
+  
   const setTask = useCallback((id: string) => {
     const params = new URLSearchParams(searchParams.toString())
 
@@ -40,38 +65,130 @@ export function Tasks({
 
     router.push(`/?${params.toString()}`)
   },[searchParams])
-  
+
+  const createTask = useCallback( async (name: string) => {        
+    fetch("/api/tasks", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json", 
+      },
+      body: JSON.stringify({
+       name,
+       projectId: currentProject 
+      }),
+    }).then(response =>  response.json())
+    .then(task => {  
+      toast(task.message ?? "A tarefa foi criada com sucesso")
+      if(task.message) return
+      setData((oldData) => [task, ...oldData])
+    })
+    .catch(error => {
+      console.error('Erro ao criar uma tarefa:', error);
+    });
+  }, [setData, currentProject])
+  const editTask = useCallback(async (name: string) => {
+    fetch(`/api/tasks/${currentTask}`, {
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json", 
+      },
+      body: JSON.stringify({
+       name
+      }),
+    }).then(response =>  response.json())
+    .then(task => {  
+      toast(task.message ?? "O nome da tarefa foi alterado com sucesso")
+      if(task.message) return
+      const index = data.findIndex((data) => data.id === currentTask) 
+      const oldData = data
+      oldData[index].name = name
+      setData(oldData)
+      setOpenEdit(false)
+    })
+    .catch(error => {
+      console.error('Erro ao alterar o nome de uma tarefa:', error);
+    });
+  },[setData, data, currentTask])
+  const deleteTask = useCallback(async () => {
+    fetch(`/api/tasks/${currentTask}`, {
+      method: 'DELETE',
+    }).then(response =>  response.json())
+    .then(task => {  
+      toast(task.message ?? "A tarefa foi deletado com sucesso")
+      if(task.message) return
+
+      setData((oldData) => oldData?.filter((project)=> project.id != currentTask))
+
+      const params = new URLSearchParams(searchParams.toString())
+
+      params.delete("task")
+
+      router.push(`/?${params.toString()}`)
+    })
+    .catch(error => {
+      console.error('Erro ao deletar um projeto:', error);
+    });
+  },[setData, searchParams, currentTask])
+
   return(
    <>
     <section className="mt-8 ml-5">
-      <h2 className="font-poppins text-neutral-800 font-semibold text-3xl sm:text-4xld mb-2">
+      <h2 className="font-poppins text-neutral-800 font-semibold text-3xl sm:text-4xl mb-2">
         Minhas Tarefas
       </h2>
 
       <div className="flex gap-4 px-1">
-        <Button
-          type="button"
-          className="min-w-24 min-h-24"
-          variant="floating"
-          size="floating"
+       <CreateTask 
+          createTask={createTask}
+          title="Nova Tarefa" 
+          description="Crie agora o sua nova tarefa! Insira o nome e comece a dar forma à organização do seu tempo." 
+          placeholder="Ex: Verbo To Be, Estudar tempos verbais..."
         >
-          <PlusIcon className="w-12 h-12 text-white" />
-        </Button>
+          <Button
+            type="button"
+            className="min-w-24 min-h-24"
+            variant="floating"
+            size="floating"
+          >
+            <PlusIcon className="w-12 h-12 text-white" />
+          </Button>
+        </CreateTask>
+
         <HorizontalScroller>
           {data.map((task, index) => {
-            if(searchParams.get('project') === task.projectId){
-              return (
-                <CardTask onClick={() => {
-                  setTask(task.id)
-                }} 
-                select={currentTask === task.id ? true : false}
-                name={task.name} key={index} />
-              )
-            }
+            return (
+              <div key={index}>
+                <CardTask 
+                  onContextMenu={(e)=> {
+                    setTask(task.id)
+                    handleContextMenu(e)
+                  }}
+                  onClick={() => {
+                    setTask(task.id)
+                  }} 
+                  select={currentTask === task.id ? true : false}
+                  name={task.name} key={index} 
+                />
+              <ContextMenu visible={visible} menuRef={menuRef} position={position} >
+                <ContextMenuButton onClick={() => setOpenEdit(true)}>Alterar Nome</ContextMenuButton>
+                <ContextMenuButton onClick={() => setOpenDelete(true)}>Deletar Projeto</ContextMenuButton>
+                <ContextMenuButton onClick={() => setOpenEdit(true)}>Ver Gráfico</ContextMenuButton>
+              </ContextMenu>
+            </div>
+            )
           })}
         </HorizontalScroller>
       </div>
     </section>
+
+    <EditTask editTask={editTask} open={openEdit} onOpenChange={setOpenEdit} 
+      title="Alterar nome" 
+      description={`Deseja realmente alterar o nome dessa tarefa ${data.find((data)=> data.id === currentTask)?.name} ?`} 
+      />
+    <DeleteTask open={openDelete} onOpenChange={setOpenDelete} deleteTask={deleteTask} 
+      title="Deletar Tarefa" 
+      description={`Deseja realmente deletar essa tarefa ${data.find((data)=> data.id === currentTask)?.name} ?`} 
+    />
 
     <TaskDetail task={data.find((task)=> task.id === currentTask)}/>
    </>
