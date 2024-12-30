43d82f322a9c840a552c1c523f7da5ad54aab406
Lucas Kauã <minifakdev15@gmail.com>
2024-12-30 13:21:09 -0300

    :recycle: refactor: Melhorando cronometro

--- src/app/(privada)/cronometro/_components/TimerTask.tsx
+++ src/app/(privada)/cronometro/_components/TimerTask.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { ChevronDown, ChevronUp } from "lucide-react";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import { useSearchParams } from "next/navigation";
 
@@ -9,11 +9,20 @@ import { Button } from "@/components/ui/Button";
 import Timer from "@/components/ui/Time";
 
 
+interface TimerProps {
+  status: 'Stopped' |'Running'|'Paused';
+  time: number;
+  name: string;
+  lastTimeSaved: number;
+}
 export function StopWatchTask(){
-  const [name, setName] = useState("...")
-  const [status, setStatus] = useState<'Stopped' |'Running'|'Paused'>("Stopped")
-  const [timer, setTimer] = useState(60)
-  const [lastTimeSaved, setLastTimeSaved] = useState(timer)
+
+  const [timer, setTimer] = useState<TimerProps>({
+    lastTimeSaved: 60,
+    status: 'Stopped',
+    time: 60,
+    name: "..."
+  })
 
   const searchParams = useSearchParams()
   const currentProject  = searchParams.get('project')
@@ -23,7 +32,7 @@ export function StopWatchTask(){
 
   useEffect(()=> {
     const getData = async() => {
-      setName(await fetch(`${process.env.NEXT_PUBLIC_BASE_URL_API}/api/tasks/${currentTask}`, {
+      const name = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL_API}/api/tasks/${currentTask}`, {
         method: "GET",
         headers: {
           'Content-Type': 'application/json',
@@ -31,15 +40,16 @@ export function StopWatchTask(){
         credentials: 'include', 
         next: { revalidate: 300 }
       }).then((res) => res.json()).then((data)=> {                
-    window.Notification.requestPermission()
         return data.name
       }).catch(error => {
         console.error('[Erro ao buscar dados das tarefa:', error);
-        return ""
-      }))
+        return "Desconhecido"
+      })
+      setTimer((timer)=> {return {...timer, name}})
+      window.Notification.requestPermission()
     }
     const getTime = async() => {
-      const localTime = localStorage.getItem("time")
+      const localTime = localStorage.getItem("timer")
 
       if(!localTime) return 
       
@@ -47,66 +57,66 @@ export function StopWatchTask(){
 
       if(lastTime.projectId !== currentProject || lastTime.taskId !== currentTask) return
 
-      setTimer(lastTime.time)
-      setLastTimeSaved(lastTime.lastTimeSaved)
+      setTimer((timer)=> {return {...timer, time: lastTime.time, lastTimeSaved: lastTime.lastTimeSaved}})
     }
     getData()
     getTime()
   }, [])
   
   useEffect(() => {
-    if (status === "Running" && !intervalRef.current) {
+    if (timer.status === "Running" && !intervalRef.current) {
       intervalRef.current = setInterval(() => {
-        const newTimer = timer - 1; 
+        const newTimer = timer.time - 1; 
 
-        if (newTimer % 10 === 0) {
+        if (newTimer % 2 === 0) {
+          console.log("SALVANDO NO BANCO");
+          
           fetch(`${process.env.NEXT_PUBLIC_BASE_URL_API}/api/projects/${currentProject}/tasks/${currentTask}`, {
             method: 'PATCH',
             headers: {
               "Content-Type": "application/json",
             },
             body: JSON.stringify({
-              addedTime: lastTimeSaved - newTimer,
+              addedTime: timer.lastTimeSaved - newTimer,
               date: new Date(),
             }),
           });
-          setLastTimeSaved(newTimer);
+          setTimer((timer) => {
+            return {...timer, lastTimeSaved: newTimer};
+          })
         }
 
-        setTimer(newTimer);
-        
-        localStorage.setItem("time", JSON.stringify({
-          projectId: currentProject,
-          taskId: currentTask,
-          time: newTimer,
-          lastTimeSaved: newTimer%10 === 0 ? newTimer  : lastTimeSaved,
-        }));
-
         if (newTimer === 0) {
-          setStatus("Stopped"); 
           window.Notification.requestPermission().then((permission) => {
             if (permission === 'granted') {
               new Notification('MindTasker', {
                 lang: 'pt-BR',
                 icon: '/favicon.ico',
-                body: `O cronometro da tarefa "${name}" chegou ao fim!`,
+                body: `O cronometro da tarefa "${timer.name}" chegou ao fim!`,
                 requireInteraction: true,
               });
             }
           })
         }
+
+        localStorage.setItem("timer", JSON.stringify({
+          projectId: currentProject,
+          taskId: currentTask,
+          time: newTimer,
+          lastTimeSaved: newTimer%10 === 0 ? newTimer  : timer.lastTimeSaved,
+        }));
+
+        setTimer((timer) => {
+          return {
+            ...timer, 
+            time: newTimer,
+            status: newTimer === 0 ? "Stopped" : "Running",
+            lastTimeSaved: newTimer%2 === 0 ? newTimer  : timer.lastTimeSaved};
+        })
       }, 60000); 
     }
-
-    console.log("intervalo", intervalRef.current);
-    console.log("status", status);
-    console.log("timer", timer);
-    console.log("lastTimeSaved", lastTimeSaved);
     
-    
-    
-    
-    if (status !== "Running" && intervalRef.current) {
+    if (timer.status !== "Running" && intervalRef.current) {
       clearInterval(intervalRef.current);
       intervalRef.current = null;
     }
@@ -116,22 +126,62 @@ export function StopWatchTask(){
       clearInterval(intervalRef.current);
       intervalRef.current = null;
     };
-  }, [status, timer, lastTimeSaved]);
+  }, [timer]);
+
+
+  const handleReset = useCallback(() => {
+    setTimer((timer) => {
+      return {...timer, lastTimeSaved: 60, time: 60, status: "Stopped"};
+    })
+  }, []);
+
+  const handleStart = useCallback(() => {
+    setTimer((timer) => {
+      return {...timer,status: "Running"};
+    })
+  }, []);
+
+
+  const handlePause = useCallback(() => {
+    setTimer((timer) => {
+      fetch(`${process.env.NEXT_PUBLIC_BASE_URL_API}/api/projects/${currentProject}/tasks/${currentTask}`, {
+        method: 'PATCH',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          addedTime: timer.lastTimeSaved - timer.time,
+          date: new Date(),
+        }),
+      });
+      return {...timer,status: "Paused", lastTimeSaved: timer.time};
+    })
+  }, []);
+
+  const handleReturn = useCallback(() => {
+    setTimer((timer) => {
+      return {...timer,status: "Running"};
+    })
+  }, []);
 
   return(
     <section>
-       <Timer current={timer} max={60} size={380}>
-        <p className="text-3xl font-inter text-center font-medium">{name}</p>
+       <Timer current={timer.time} max={60} size={380}>
+        <p className="text-3xl font-inter text-center font-medium">{timer.name}</p>
         <div className="my-4">  
-          <div className={`flex w-80 ${status !== "Stopped" ? "justify-center" : " justify-between"}`}>
-            {status === "Stopped" && (
-              <button disabled={timer <= 0} className={timer === 0 ? "cursor-not-allowed text-black/50" : ""} onClick={() => setTimer(timer >= 5 ? timer - 5 : 0)}>
+          <div className={`flex w-80 ${timer.status !== "Stopped" ? "justify-center" : " justify-between"}`}>
+            {timer.status === "Stopped" && (
+              <button disabled={timer.time <= 0} className={timer.time  === 0 ? "cursor-not-allowed text-black/50" : ""} 
+                onClick={() => setTimer((timer) => { return {...timer, time: timer.time - 5, lastTimeSaved: timer.time - 5} })}
+              >
                 <ChevronDown className="w-16 h-16"/>
               </button>
             )}
-            <p className="font-poppins text-6xl font-semibold">{timer}</p>
-            {status === "Stopped" && (
-              <button disabled={timer === 60} className={timer === 60 ? "cursor-not-allowed text-black/50" : ""} onClick={() => setTimer(timer + 1)} >
+            <p className="font-poppins text-6xl font-semibold">{timer.time}</p>
+            {timer.status === "Stopped" && (
+              <button disabled={timer.time  === 60} className={timer.time === 60 ? "cursor-not-allowed text-black/50" : ""} 
+                onClick={() => setTimer((timer) => { return {...timer, time: timer.time + 1, lastTimeSaved: timer.time + 1} })}
+               >
                 <ChevronUp className="w-16 h-16"/>
               </button>
             )}
@@ -139,18 +189,18 @@ export function StopWatchTask(){
           <p className="text-2xl font-inter text-center">minutos</p>
         </div>
         <div className="m-auto w-64 flex justify-between items-center">
-        {status === "Stopped" ? (
+        {timer.status === "Stopped" ? (
           <>
-            <Button onClick={()=> setTimer(60)} type="button">Resetar</Button>
-            <Button onClick={()=> setStatus("Running")} type="button">Iniciar</Button>
+            <Button onClick={handleReset} type="button">Resetar</Button>
+            <Button onClick={handleStart} type="button">Iniciar</Button>
           </>
         ): (
           <>
-            <Button onClick={()=> setStatus("Stopped")} type="button">Parar</Button>
-            {status === "Paused" ? (
-              <Button onClick={()=> setStatus("Running")} type="button">Voltar</Button>
+            <Button onClick={handleReset} type="button">Parar</Button>
+            {timer.status === "Paused" ? (
+              <Button onClick={handleReturn} type="button">Voltar</Button>
             ) : (
-            <Button onClick={()=> setStatus("Paused")} type="button">Pausar</Button>
+              <Button onClick={handlePause} type="button">Pausar</Button>
             )}
           </>
         )}
