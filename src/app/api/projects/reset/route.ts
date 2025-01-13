3ac51012fa379f8010df402d5c3cd823d64a89cd
Lucas Kauã <minifakdev15@gmail.com>
2025-01-13 13:47:18 -0300

    🐛 fix: Faltando Retorno do NextResponse

--- src/app/api/projects/reset/route.ts
+++ src/app/api/projects/reset/route.ts
@@ -1,36 +1,36 @@
-import { db } from '@/services/FirebaseConnection'
-import { collection, getDocs, updateDoc } from 'firebase/firestore'
-import { NextResponse } from 'next/server'
+import { db } from "@/services/FirebaseConnection";
+import { collection, getDocs, updateDoc } from "firebase/firestore";
+import { NextResponse } from "next/server";
 
-export async function GET(request: Request){
-  try{
+export async function GET(request: Request) {
+  try {
+    if (new Date().getDay() !== 1) throw new Error("Não pode ser executado");
+    const projects = await getDocs(collection(db, "projects"));
 
-    if(new Date().getDay() !== 1) throw new Error("Não pode ser executado")
-    const projects = await getDocs(collection(db, "projects"))
-
-    projects.forEach(async(doc) => {
+    projects.forEach(async (doc) => {
       await updateDoc(doc.ref, {
-        month: Array.from({length: 31}, () => 0),
-        ...(new Date().getMonth() === 0 ? { year: Array.from({length: 12}, () => 0) } : {}),
-      })
-    })
+        month: Array.from({ length: 31 }, () => 0),
+        ...(new Date().getMonth() === 0
+          ? { year: Array.from({ length: 12 }, () => 0) }
+          : {}),
+      });
+    });
 
-    const tasks = await getDocs(collection(db, "tasks"))
+    const tasks = await getDocs(collection(db, "tasks"));
 
-    tasks.forEach(async(doc) => {
+    tasks.forEach(async (doc) => {
       await updateDoc(doc.ref, {
-        month: Array.from({length: 31}, () => 0),   
-        ...(new Date().getMonth() === 0 ? { year: Array.from({length: 12}, () => 0) } : {}),
-      })
-    })
+        month: Array.from({ length: 31 }, () => 0),
+        ...(new Date().getMonth() === 0
+          ? { year: Array.from({ length: 12 }, () => 0) }
+          : {}),
+      });
+    });
 
-    NextResponse.json({ message: "Projeto resetado com sucesso" })
+    return NextResponse.json({ message: "Projeto resetado com sucesso" });
   } catch (error) {
-    if(error instanceof Error){
-      return NextResponse.json(
-        { message: error.message },
-        { status: 500 }
-      );
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 500 });
     }
     return NextResponse.json(
       { message: "Houve um erro ao tentar ver todos os projetos" },
