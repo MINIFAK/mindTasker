8900100ec4fc7700bad37620872fee4349ac79f7
Lucas Kauã <minifakdev15@gmail.com>
2025-01-21 11:23:21 -0300

    ✨ feat: Não partir tarefa errado ou tarefa que não é sua

--- src/app/(privada)/cronometro/_components/TimerTask.tsx
+++ src/app/(privada)/cronometro/_components/TimerTask.tsx
@@ -3,19 +3,19 @@
 import { ChevronDown, ChevronUp } from "lucide-react";
 import { useCallback, useEffect, useRef, useState } from "react";
 
-import { useSearchParams } from "next/navigation";
+import { notFound, useRouter, useSearchParams } from "next/navigation";
 
 import { Button } from "@/components/ui/Button";
 import Timer from "@/components/ui/Time";
 
 
 interface TimerProps {
-  status: 'Stopped' |'Running'|'Paused';
+  status: 'Stopped' | 'Running' | 'Paused';
   time: number;
   name: string;
   lastTimeSaved: number;
 }
-export function StopWatchTask(){
+export function StopWatchTask() {
 
   const [timer, setTimer] = useState<TimerProps>({
     lastTimeSaved: 60,
@@ -24,53 +24,55 @@ export function StopWatchTask(){
     name: "..."
   })
 
+  const router = useRouter()
   const searchParams = useSearchParams()
-  const currentProject  = searchParams.get('project')
-  const currentTask  = searchParams.get('task')
+  const currentProject = searchParams.get('project')
+  const currentTask = searchParams.get('task')
 
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
-  useEffect(()=> {
-    const getData = async() => {
+  useEffect(() => {
+    const getData = async () => {
       const name = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL_API}/api/tasks/${currentTask}`, {
         method: "GET",
         headers: {
           'Content-Type': 'application/json',
         },
-        credentials: 'include', 
+        credentials: 'include',
         next: { revalidate: 300 }
-      }).then((res) => res.json()).then((data)=> {                
+      }).then((res) => res.json()).then((data) => {
+        console.log(data)
+        if (data.message) return router.push("/dashboard")
         return data.name
-      }).catch(error => {
-        console.error('[Erro ao buscar dados das tarefa:', error);
+      }).catch(() => {
         return "Desconhecido"
       })
-      setTimer((timer)=> {return {...timer, name}})
+      setTimer((timer) => { return { ...timer, name } })
       window.Notification.requestPermission()
     }
-    const getTime = async() => {
+    const getTime = async () => {
       const localTime = localStorage.getItem("timer")
 
-      if(!localTime) return 
-      
+      if (!localTime) return
+
       const lastTime = JSON.parse(localTime) as { projectId: string, taskId: string, time: number; lastTimeSaved: number }
 
-      if(lastTime.projectId !== currentProject || lastTime.taskId !== currentTask) return
+      if (lastTime.projectId !== currentProject || lastTime.taskId !== currentTask) return
 
-      setTimer((timer)=> {return {...timer, time: lastTime.time, lastTimeSaved: lastTime.lastTimeSaved}})
+      setTimer((timer) => { return { ...timer, time: lastTime.time, lastTimeSaved: lastTime.lastTimeSaved } })
     }
     getData()
     getTime()
   }, [])
-  
+
   useEffect(() => {
     if (timer.status === "Running" && !intervalRef.current) {
       intervalRef.current = setInterval(() => {
-        const newTimer = timer.time - 1; 
+        const newTimer = timer.time - 1;
 
         if (newTimer % 2 === 0) {
           console.log("SALVANDO NO BANCO");
-          
+
           fetch(`${process.env.NEXT_PUBLIC_BASE_URL_API}/api/projects/${currentProject}/tasks/${currentTask}`, {
             method: 'PATCH',
             headers: {
@@ -82,7 +84,7 @@ export function StopWatchTask(){
             }),
           });
           setTimer((timer) => {
-            return {...timer, lastTimeSaved: newTimer};
+            return { ...timer, lastTimeSaved: newTimer };
           })
         }
 
@@ -103,19 +105,20 @@ export function StopWatchTask(){
           projectId: currentProject,
           taskId: currentTask,
           time: newTimer,
-          lastTimeSaved: newTimer%10 === 0 ? newTimer  : timer.lastTimeSaved,
+          lastTimeSaved: newTimer % 10 === 0 ? newTimer : timer.lastTimeSaved,
         }));
 
         setTimer((timer) => {
           return {
-            ...timer, 
+            ...timer,
             time: newTimer,
             status: newTimer === 0 ? "Stopped" : "Running",
-            lastTimeSaved: newTimer%2 === 0 ? newTimer  : timer.lastTimeSaved};
+            lastTimeSaved: newTimer % 2 === 0 ? newTimer : timer.lastTimeSaved
+          };
         })
-      }, 60000); 
+      }, 60000);
     }
-    
+
     if (timer.status !== "Running" && intervalRef.current) {
       clearInterval(intervalRef.current);
       intervalRef.current = null;
@@ -131,13 +134,13 @@ export function StopWatchTask(){
 
   const handleReset = useCallback(() => {
     setTimer((timer) => {
-      return {...timer, lastTimeSaved: 60, time: 60, status: "Stopped"};
+      return { ...timer, lastTimeSaved: 60, time: 60, status: "Stopped" };
     })
   }, []);
 
   const handleStart = useCallback(() => {
     setTimer((timer) => {
-      return {...timer,status: "Running"};
+      return { ...timer, status: "Running" };
     })
   }, []);
 
@@ -154,57 +157,57 @@ export function StopWatchTask(){
           date: new Date(),
         }),
       });
-      return {...timer,status: "Paused", lastTimeSaved: timer.time};
+      return { ...timer, status: "Paused", lastTimeSaved: timer.time };
     })
   }, []);
 
   const handleReturn = useCallback(() => {
     setTimer((timer) => {
-      return {...timer,status: "Running"};
+      return { ...timer, status: "Running" };
     })
   }, []);
 
-  return(
+  return (
     <section>
-       <Timer current={timer.time} max={60} size={380}>
+      <Timer current={timer.time} max={60} size={380}>
         <p className="text-3xl font-inter text-center font-medium">{timer.name}</p>
-        <div className="my-4">  
+        <div className="my-4">
           <div className={`flex w-80 ${timer.status !== "Stopped" ? "justify-center" : " justify-between"}`}>
             {timer.status === "Stopped" && (
-              <button disabled={timer.time <= 0} className={timer.time  === 0 ? "cursor-not-allowed text-black/50" : ""} 
-                onClick={() => setTimer((timer) => { return {...timer, time: timer.time - 5, lastTimeSaved: timer.time - 5} })}
+              <button disabled={timer.time <= 0} className={timer.time === 0 ? "cursor-not-allowed text-black/50" : ""}
+                onClick={() => setTimer((timer) => { return { ...timer, time: timer.time - 5, lastTimeSaved: timer.time - 5 } })}
               >
-                <ChevronDown className="w-16 h-16"/>
+                <ChevronDown className="w-16 h-16" />
               </button>
             )}
             <p className="font-poppins text-6xl font-semibold">{timer.time}</p>
             {timer.status === "Stopped" && (
-              <button disabled={timer.time  === 60} className={timer.time === 60 ? "cursor-not-allowed text-black/50" : ""} 
-                onClick={() => setTimer((timer) => { return {...timer, time: timer.time + 1, lastTimeSaved: timer.time + 1} })}
-               >
-                <ChevronUp className="w-16 h-16"/>
+              <button disabled={timer.time === 60} className={timer.time === 60 ? "cursor-not-allowed text-black/50" : ""}
+                onClick={() => setTimer((timer) => { return { ...timer, time: timer.time + 1, lastTimeSaved: timer.time + 1 } })}
+              >
+                <ChevronUp className="w-16 h-16" />
               </button>
             )}
           </div>
           <p className="text-2xl font-inter text-center">minutos</p>
         </div>
         <div className="m-auto w-64 flex justify-between items-center">
-        {timer.status === "Stopped" ? (
-          <>
-            <Button onClick={handleReset} type="button">Resetar</Button>
-            <Button onClick={handleStart} type="button">Iniciar</Button>
-          </>
-        ): (
-          <>
-            <Button onClick={handleReset} type="button">Parar</Button>
-            {timer.status === "Paused" ? (
-              <Button onClick={handleReturn} type="button">Voltar</Button>
-            ) : (
-              <Button onClick={handlePause} type="button">Pausar</Button>
-            )}
-          </>
-        )}
-        </div>  
+          {timer.status === "Stopped" ? (
+            <>
+              <Button onClick={handleReset} type="button">Resetar</Button>
+              <Button onClick={handleStart} type="button">Iniciar</Button>
+            </>
+          ) : (
+            <>
+              <Button onClick={handleReset} type="button">Parar</Button>
+              {timer.status === "Paused" ? (
+                <Button onClick={handleReturn} type="button">Voltar</Button>
+              ) : (
+                <Button onClick={handlePause} type="button">Pausar</Button>
+              )}
+            </>
+          )}
+        </div>
       </Timer>
     </section>
   )
--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,19 +1,27 @@
+import { db } from "@/services/FirebaseConnection";
+import { deleteDoc, doc, getDoc, updateDoc } from "firebase/firestore";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 
-import { db } from '@/services/FirebaseConnection';
-import { deleteDoc, doc, getDoc, updateDoc } from 'firebase/firestore';
-import { getServerSession } from 'next-auth';
-import { NextRequest, NextResponse } from 'next/server'
-
-export async function GET(req: NextRequest, { params }: { params: { id: string } }){
-  try{
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
     const session = await getServerSession();
-    
-    if(!session || !session.user?.email) throw new Error("Você precisa estar autenticado para acessar esses dados.")
-    
-    const docRef = await getDoc(doc(db, "tasks", params.id))
 
-    if(!docRef || docRef.data()?.email !== session.user.email) throw new Error("Esse tarefa não foi encontrada")
-    
+    if (!session || !session.user?.email)
+      throw new Error(
+        "Você precisa estar autenticado para acessar esses dados."
+      );
+
+    const docRef = await getDoc(doc(db, "tasks", params.id));
+
+    if (!docRef) throw new Error("Esse tarefa não foi encontrada");
+
+    if (docRef.data()?.email !== session.user.email)
+      throw new Error("Esse tarefa não pertence a vocẽ");
+
     return NextResponse.json({
       id: docRef.id,
       name: docRef.data()?.name,
@@ -21,13 +29,9 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
       month: docRef.data()?.month,
       year: docRef.data()?.year,
     });
-
-    } catch (error) {
-    if(error instanceof Error){
-      return NextResponse.json(
-        { message: error.message },
-        { status: 500 }
-      );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 500 });
     }
     return NextResponse.json(
       { message: "Houve um erro ao tentar ver uma tarefa" },
@@ -36,21 +40,24 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
   }
 }
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }){
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     const session = await getServerSession();
-    
-    if(!session || !session.user?.email) throw new Error("Você precisa estar autenticado para acessar esses dados.")
-    
-    await deleteDoc(doc(db, "tasks", params.id))
 
-    return NextResponse.json({ id: params.id })
-  } catch (error) {
-    if(error instanceof Error){
-      return NextResponse.json(
-        { message: error.message },
-        { status: 500 }
+    if (!session || !session.user?.email)
+      throw new Error(
+        "Você precisa estar autenticado para acessar esses dados."
       );
+
+    await deleteDoc(doc(db, "tasks", params.id));
+
+    return NextResponse.json({ id: params.id });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 500 });
     }
     return NextResponse.json(
       { message: "Houve um erro ao tentar deletar uma tarefa" },
@@ -59,27 +66,30 @@ export async function DELETE(req: NextRequest, { params }: { params: { id: strin
   }
 }
 
-export async function PATCH(req: NextRequest, { params }: { params: { id: string } }){
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     const session = await getServerSession();
-    
-    if(!session || !session.user?.email) throw new Error("Você precisa estar autenticado para acessar esses dados.")
-    
+
+    if (!session || !session.user?.email)
+      throw new Error(
+        "Você precisa estar autenticado para acessar esses dados."
+      );
+
     const { name } = await req.json();
 
-    if(!name) throw new Error("Necessario enviar o nome da tarefa")
-    
+    if (!name) throw new Error("Necessario enviar o nome da tarefa");
+
     await updateDoc(doc(db, "tasks", params.id), {
       name,
-    })
-    
-    return NextResponse.json({ id: params.id, name: name })
+    });
+
+    return NextResponse.json({ id: params.id, name: name });
   } catch (error) {
-    if(error instanceof Error){
-      return NextResponse.json(
-        { message: error.message },
-        { status: 500 }
-      );
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 500 });
     }
     return NextResponse.json(
       { message: "Houve um erro ao tentar atualizar o nome da tarefa" },
