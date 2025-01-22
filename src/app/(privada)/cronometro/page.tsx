b51207990fefcdffaf55ffbe7fed385eee38318d
Lucas Kauã <minifakdev15@gmail.com>
2025-01-22 11:58:44 -0300

    🐛 fix: Colocando Suspense no Timer

--- src/app/(privada)/cronometro/page.tsx
+++ src/app/(privada)/cronometro/page.tsx
@@ -1,16 +1,20 @@
 import { Header } from "@/components/layout/Header";
 import { StopWatchTask } from "./_components/TimerTask";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: 'Cronometro',
 }
+
 export default async function Cronometro() {
   return (
     <div className="flex flex-col min-h-dvh">
       <Header />
       <main className="flex-1 p-2 xl:px-28 overflow-auto flex justify-center items-center">
-        <StopWatchTask />
+        <Suspense fallback={<div>Carregando...</div>}>
+          <StopWatchTask />
+        </Suspense>
       </main>
     </div>
   );
