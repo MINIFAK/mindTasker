16d2af30d5ba285f1ac0c15d6cc3022a8af72ae5
Lucas Kauã <minifakdev15@gmail.com>
2025-01-29 18:30:04 -0300

    ✨ feat: Middleware para sair do login caso logado

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,19 +3,23 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt"; // Importa a função getToken para obter o JWT
 import { NextRequest } from "next/server"; // Importa NextRequest
 
-
 export const config = {
-  matcher: ["/dashboard/:path*", "/cronometro/:path*"],
+  matcher: [
+    "/dashboard/:path*",
+    "/cronometro/:path*",
+    "/grafico/:path*",
+    "/login",
+  ],
 };
 export default async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET }) 
-  
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url)); 
-  }    
-  if (token && req.url.includes("/login")) {
-    return NextResponse.redirect(new URL("/", req.url)); 
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (!token && !req.url.includes("/login")) {
+    return NextResponse.redirect(new URL("/login", req.url));
   }
-  
+  if (token && req.url.includes("/login")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
   return NextResponse.next();
 }
